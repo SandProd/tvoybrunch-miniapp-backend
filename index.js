76d1976dfdd9244980661eeffbbf861530dfa484
Mykyta Tishkin <87891550+GTeasera@users.noreply.github.com>
2023-11-15 12:21:23 +0200

    initial

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const TelegramBot = require('node-telegram-bot-api');
 
 const token = '6835736852:AAGJL4zqg5Qd8aE7Di2zaXm5ccuZE9RNa5Y';
-const webAppUrl = '/';
+const webAppUrl = 'https://www.google.com/';
 const bot = new TelegramBot(token, {polling:true});
 
 bot.on('message', async (msg) => {
@@ -12,7 +12,7 @@ bot.on('message', async (msg) => {
         await bot.sendMessage(chatId, 'Bottom button, fill form', {
             reply_markup: {
                 inline_keyboard: [
-                    [{text: 'Make order', web_app: {webAppUrl}}]
+                    [{text: 'Make order', web_app: {url:webAppUrl}}]
                 ]
             }
         })

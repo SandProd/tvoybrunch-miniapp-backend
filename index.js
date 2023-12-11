fbf996690018234c410355136d5e18efff497aa3
main-del <vajgleb03@gmail.com>
2023-12-11 19:03:01 +0300

    Cleaned-up and organized

--- index.js
+++ index.js
@@ -1,104 +1,100 @@
-const HTTP = require('http');
+const http = require('http');
 const TelegramBot = require("node-telegram-bot-api");
 const express = require('express');
 const cors = require('cors');
-const mysql = require('mysql')
+const mysql = require('mysql');
+
 const token = '6835736852:AAGJL4zqg5Qd8aE7Di2zaXm5ccuZE9RNa5Y';
 const webAppUrl = 'https://rococo-lily-4bd96e.netlify.app';
 
 const PORT = '3000';
-const HOSTNAME = '127.1.1.141'; 
+const HOSTNAME = '127.1.1.141';
 
-
-const bot = new TelegramBot(token, {polling: true});
+const bot = new TelegramBot(token, { polling: true });
 const app = express();
 app.use(express.static('public'));
 app.use(express.json());
 app.use(cors());
 
-
 let Username;
 
+// Bot event handling
 bot.on('message', (message) => {
-  const username = message.chat.first_name;
-  console.log(`User ${username} is interacting with the bot.`);
-  globalUsername = username;
+    const username = message.chat.first_name;
+    console.log(`User ${username} is interacting with the bot.`);
+    Username = username;
 });
 
 bot.on('message', async (msg) => {
-    console.log('Server started')
     const chatId = msg.chat.id;
     const text = msg.text;
 
-    if(text === '/start') {
+    if (text === '/start') {
         await bot.sendMessage(chatId, 'Ниже появится кнопка, заполни форму ', {
             reply_markup: {
                 keyboard: [
-                    [{text: 'Адресс доставки', web_app: {url: webAppUrl + '/form'}}]
+                    [{ text: 'Адресс доставки', web_app: { url: `${webAppUrl}/form` } }]
                 ]
             }
-        })
+        });
 
         await bot.sendMessage(chatId, 'Заходи в наш интернет магазин по кнопке ниже', {
             reply_markup: {
                 inline_keyboard: [
-                    [{text: '🍱 Меню', web_app: {url: webAppUrl}}],
-                    [{text: '🎟️ Акции', web_app: {url: webAppUrl + '/discounts'}}],
-                    [{text: '👤 Профиль', web_app: {url: webAppUrl + '/profile'}}],
-                    [{text: '📱 Контакты', web_app: {url: webAppUrl + '/contacts'}}]
+                    [{ text: '🍱 Меню', web_app: { url: webAppUrl } }],
+                    [{ text: '🎟️ Акции', web_app: { url: `${webAppUrl}/discounts` } }],
+                    [{ text: '👤 Профиль', web_app: { url: `${webAppUrl}/profile` } }],
+                    [{ text: '📱 Контакты', web_app: { url: `${webAppUrl}/contacts` } }]
                 ]
             }
-        })
+        });
     }
 
-    if(msg?.web_app_data?.data) {
+    if (msg?.web_app_data?.data) {
         try {
-            const data = JSON.parse(msg?.web_app_data?.data)
-            console.log(data)
-            await bot.sendMessage(chatId, 'Спасибо за обратную связь!')
+            const data = JSON.parse(msg?.web_app_data?.data);
+            console.log(data);
+            await bot.sendMessage(chatId, 'Спасибо за обратную связь!');
             await bot.sendMessage(chatId, 'Ваш номер дома: ' + data?.country);
             await bot.sendMessage(chatId, 'Ваша улица: ' + data?.street);
 
             setTimeout(async () => {
                 await bot.sendMessage(chatId, 'Всю информацию вы получите в этом чате');
-            }, 3000)
+            }, 3000);
         } catch (e) {
             console.log(e);
         }
     }
 });
 
-
-
-
+// Database connection
 const connection = mysql.createConnection({
-    host:'tvoybruc.mysql.tools',
-    user:'tvoybruc_db',
-    password:'wjtMG2Wc',
+    host: 'tvoybruc.mysql.tools',
+    user: 'tvoybruc_db',
+    password: 'wjtMG2Wc',
     database: 'tvoybruc_db'
-})
+});
 
-connection.connect(function(err) {
+connection.connect((err) => {
     if (err) throw err;
     console.log("Connected!");
     const sql = "SELECT * FROM Orders";
-    connection.query(sql, function (err, result) {
-      if (err) throw err;
-      console.log("Result: ");
-      console.log(result);
+    connection.query(sql, (err, result) => {
+        if (err) throw err;
+        console.log("Result: ");
+        console.log(result);
     });
-  });
+});
 
-
-
+// Server route
 app.post('/web-data', async (req, res) => {
-    const {queryId, products = [], totalPrice} = req.body;
+    const { queryId, products = [], totalPrice } = req.body;
     try {
-        var sql = `INSERT INTO Orders (username, userorder, TotalPrice) VALUES ('${Username}', '${products.map(item => item.title).join(', ')}', ${totalPrice})`
-        connection.query(sql,function(err,result){
+        const sql = `INSERT INTO Orders (username, userorder, TotalPrice) VALUES ('${Username}', '${products.map(item => item.title).join(', ')}', ${totalPrice})`;
+        connection.query(sql, (err, result) => {
             if (err) throw err;
-            console.log("1 record inserted")
-        })
+            console.log("1 record inserted");
+        });
 
         await bot.answerWebAppQuery(queryId, {
             type: 'article',
@@ -107,17 +103,16 @@ app.post('/web-data', async (req, res) => {
             input_message_content: {
                 message_text: ` Поздравляю с покупкой, вы приобрели товар на сумму ${totalPrice} BYN, ${products.map(item => item.title).join(', ')}`
             }
-        })
-        return res.status(200).json({});
-    } 
-    catch (e) {
-        input_message_content: {
-            message_text: ` Не удалось произвести покупку, вы пытались приобрели товар на сумму ${totalPrice}, ${products.map(item => item.title).join(', ')}`
-        }
-        return res.status(500).json({})
-    }
-})
+        });
 
+        return res.status(200).json({});
+    } catch (e) {
+        console.log(e);
+        return res.status(500).json({});
+    }
+});
+
+// Server listening
 app.listen(PORT, HOSTNAME, () => {
     console.log(`Server started on ${HOSTNAME}:${PORT}`);
 });

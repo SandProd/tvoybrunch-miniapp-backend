4274e54aaf0278a43b01e5986ba6861eee32eee9
main-del <vajgleb03@gmail.com>
2023-12-17 14:02:57 +0300

    products crud

--- index.js
+++ index.js
@@ -113,20 +113,97 @@ app.post('/web-data', async (req, res) => {
     }
 });
 
+// Добавление нового продукта
+app.post('/products', (req, res) => {
+    const { name, price } = req.body;
+    const sql = 'INSERT INTO Products (name, price) VALUES (?, ?)';
+  
+    connection.query(sql, [name, price], (err, result) => {
+        if (err) {
+            console.error('Ошибка запроса к базе данных: ' + err.message);
+            res.status(500).json({ error: 'Internal Server Error' });
+            return;
+        }
+
+        res.status(201).json({ message: 'Продукт успешно добавлен' });
+    });
+});
+
+// Получение всех продуктов
 app.get('/products', (req, res) => {
     const sql = 'SELECT * FROM Products';
   
     connection.query(sql, (err, results) => {
-      if (err) {
-        console.error('Ошибка запроса к базе данных: ' + err.message);
-        res.status(500).json({ error: 'Internal Server Error' });
-        return;
-      }
-  
-      res.json(results);
-      console.log(results)
+        if (err) {
+            console.error('Ошибка запроса к базе данных: ' + err.message);
+            res.status(500).json({ error: 'Internal Server Error' });
+            return;
+        }
+
+        res.json(results);
     });
-  });
+});
+
+// Получение конкретного продукта по Id
+app.get('/products/:id', (req, res) => {
+    const productId = req.params.id;
+    const sql = 'SELECT * FROM Products WHERE Id = ?';
+  
+    connection.query(sql, [productId], (err, result) => {
+        if (err) {
+            console.error('Ошибка запроса к базе данных: ' + err.message);
+            res.status(500).json({ error: 'Internal Server Error' });
+            return;
+        }
+
+        if (result.length === 0) {
+            res.status(404).json({ error: 'Продукт не найден' });
+        } else {
+            res.json(result[0]);
+        }
+    });
+});
+
+// Обновление продукта по Id
+app.put('/products/:id', (req, res) => {
+    const productId = req.params.id;
+    const { name, price } = req.body;
+    const sql = 'UPDATE Products SET name = ?, price = ? WHERE Id = ?';
+  
+    connection.query(sql, [name, price, productId], (err, result) => {
+        if (err) {
+            console.error('Ошибка запроса к базе данных: ' + err.message);
+            res.status(500).json({ error: 'Internal Server Error' });
+            return;
+        }
+
+        if (result.affectedRows === 0) {
+            res.status(404).json({ error: 'Продукт не найден' });
+        } else {
+            res.json({ message: 'Продукт успешно обновлен' });
+        }
+    });
+});
+
+// Удаление продукта по Id
+app.delete('/products/:id', (req, res) => {
+    const productId = req.params.id;
+    const sql = 'DELETE FROM Products WHERE Id = ?';
+  
+    connection.query(sql, [productId], (err, result) => {
+        if (err) {
+            console.error('Ошибка запроса к базе данных: ' + err.message);
+            res.status(500).json({ error: 'Internal Server Error' });
+            return;
+        }
+
+        if (result.affectedRows === 0) {
+            res.status(404).json({ error: 'Продукт не найден' });
+        } else {
+            res.json({ message: 'Продукт успешно удален' });
+        }
+    });
+});
 
 // Server listening
 app.listen(PORT, HOSTNAME, () => {

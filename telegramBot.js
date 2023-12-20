084dda7693e4fb04fd07ae79c811c3dd889eacc5
main-del <vajgleb03@gmail.com>
2023-12-20 21:11:21 +0300

    save form info

--- telegramBot.js
+++ telegramBot.js
@@ -1,4 +1,5 @@
 const TelegramBot = require("node-telegram-bot-api");
+const db = require("./db");
 
 const token = '6835736852:AAGJL4zqg5Qd8aE7Di2zaXm5ccuZE9RNa5Y';
 const webAppUrl = 'https://rococo-lily-4bd96e.netlify.app';
@@ -34,11 +35,30 @@ bot.on('message', async (msg) => {
         try {
             const data = JSON.parse(msg?.web_app_data?.data);
             console.log(data);
+
+            // Save or update user information in the Users table
+            const query = `
+                INSERT INTO Users (country, street, district, username)
+                VALUES (?, ?, ?, ?)
+                ON DUPLICATE KEY UPDATE
+                country = VALUES(country), street = VALUES(street), district = VALUES(district), username = VALUES(username);
+            `;
+
+            const values = [data?.country, data?.street, data?.district, data?.username];
+
+            db.query(query, values, (err, result) => {
+                if (err) {
+                    console.error('Error saving user information to the database: ' + err.stack);
+                    return;
+                }
+                console.log('User information saved to the database');
+            });
+
             await bot.sendMessage(chatId, 'Спасибо за обратную связь!');
             await bot.sendMessage(chatId, 'Ваш номер дома: ' + data?.country);
             await bot.sendMessage(chatId, 'Ваша улица: ' + data?.street);
-            await bot.sendMessage(chatId, 'Ваш район:' + data?.district);
-            await bot.sendMessage(chatId, 'Ваш юзернейм:' + data?.username);
+            await bot.sendMessage(chatId, 'Ваш район: ' + data?.district);
+            await bot.sendMessage(chatId, 'Ваш юзернейм: ' + data?.username);
 
             setTimeout(async () => {
                 await bot.sendMessage(chatId, 'Вся информация будет отправлена в этот чат');

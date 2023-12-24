15084566121b2ab238dfec62b577b733c0d796b8
main-del <vajgleb03@gmail.com>
2023-12-24 12:27:23 +0300

    save index.js :)

--- index.js
+++ index.js
@@ -2,11 +2,9 @@ const express = require('express');
 const cors = require('cors');
 const db = require('./db');
 const productsRouter = require('./routes/products');
-const { bot } = require('./telegramBot');
 const webDataRouter = require('./routes/webData');
-
-const PORT = '3000';
-const HOSTNAME = '127.1.1.141';
+const logger = require('./logger');  // Import the logger module
+const config = require('./config');
 
 const app = express();
 app.use(express.static('public'));
@@ -16,22 +14,20 @@ app.use(cors());
 // Database connection
 db.connect((err) => {
     if (err) throw err;
-    console.log("Connected!");
+    logger.info("Connected to the database");
 });
 
-// Products route
+// Routes
 app.use('/products', productsRouter);
-
-// WebData route
 app.use('/web-data', webDataRouter);
+
+// Server listening
+app.listen(config.PORT, config.HOSTNAME, () => {
+    logger.info(`Server started on ${config.HOSTNAME}:${config.PORT}`);
+});
 
 // Обработчик ошибок Express
 app.use((err, req, res, next) => {
     logger.error(err.stack);
     res.status(500).json({ error: 'Internal Server Error' });
-});
-
-// Server listening
-app.listen(PORT, HOSTNAME, () => {
-    logger.info(`Server started on ${HOSTNAME}:${PORT}`);
 });
